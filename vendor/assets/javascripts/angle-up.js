76589f1cdc8c080d974a8a85909dfb44e3ad669b
aptx4869 <ling548@gmail.com>
2013-04-05 08:57:02 +0800

    Update angle-up from upstream

--- vendor/assets/javascripts/angle-up.js
+++ vendor/assets/javascripts/angle-up.js
@@ -1,31 +1,14 @@
-
 (function() {
-  var __slice = Array.prototype.slice;
+  var inheritClazz, module,
+    __slice = [].slice,
+    _this = this;
 
-  this.Router = (function() {
+  this.RailsRouter = (function() {
 
-    Router.prototype.initRoutes = function(routes) {
-      var info, routeName;
-      for (routeName in routes) {
-        info = routes[routeName];
-        if (routeName === "default") {
-          this.$route.otherwise({
-            redirectTo: info
-          });
-        } else {
-          this.$route.when(routeName, {
-            template: info.template,
-            controller: info.controller
-          });
-        }
-      }
-      return this.$route.parent(this);
-    };
+    function RailsRouter() {}
 
-    Router.prototype.setupXhr = function() {
+    RailsRouter.prototype.setupXHR = function() {
       var token;
-      this.$xhr.defaults.headers.post['Content-Type'] = 'application/json';
-      this.$xhr.defaults.headers.put['Content-Type'] = 'application/json';
       if (token = $("meta[name='csrf-token']").attr("content")) {
         this.$xhr.defaults.headers.post['X-CSRF-Token'] = token;
         this.$xhr.defaults.headers.put['X-CSRF-Token'] = token;
@@ -33,14 +16,7 @@
       }
     };
 
-    function Router($route, $xhr) {
-      this.$route = $route;
-      this.$xhr = $xhr;
-      this.setupXhr();
-      this.initRoutes(this.routes());
-    }
-
-    return Router;
+    return RailsRouter;
 
   })();
 
@@ -85,32 +61,39 @@
     });
   };
 
-  angular.element(document).ready(function() {
-    return angular.compile(document)().$apply();
-  });
+  inheritClazz = function(obj, clazz) {
+    var key, objProto, value;
+    objProto = new clazz();
+    for (key in objProto) {
+      value = objProto[key];
+      obj[key] = value;
+    }
+    obj.constructor = objProto.constructor;
+    return typeof obj.initialize === "function" ? obj.initialize() : void 0;
+  };
 
   this.AngularModel = (function() {
 
     function AngularModel() {}
 
     AngularModel.prototype.initialize = function() {
-      var association, clazz, name, obj, _ref, _results;
+      var clazz, name, obj, _ref, _results;
       if (this.hasMany) {
         _ref = this.hasMany;
         _results = [];
         for (name in _ref) {
           clazz = _ref[name];
-          association = this[name] || [];
+          this[name] || (this[name] = []);
           _results.push((function() {
-            var _i, _len, _results2;
-            _results2 = [];
-            for (_i = 0, _len = association.length; _i < _len; _i++) {
-              obj = association[_i];
-              obj.__proto__ = new clazz();
-              _results2.push(typeof obj.initialize === "function" ? obj.initialize() : void 0);
+            var _i, _len, _ref1, _results1;
+            _ref1 = this[name];
+            _results1 = [];
+            for (_i = 0, _len = _ref1.length; _i < _len; _i++) {
+              obj = _ref1[_i];
+              _results1.push(inheritClazz(obj, clazz));
             }
-            return _results2;
-          })());
+            return _results1;
+          }).call(this));
         }
         return _results;
       }
@@ -120,7 +103,9 @@
 
   })();
 
-  angular.service("eventuallyWork", (function($defer) {
+  module = angular.module("eventuallyWork", ['$defer']);
+
+  module.factory('serviceId', function() {
     var eventuallyWork;
     eventuallyWork = function(func, timeout) {
       try {
@@ -134,15 +119,58 @@
     return function(func) {
       return eventuallyWork(func, 10);
     };
-  }), {
-    $inject: ['$defer']
   });
+
+  this.adaptForAngular = function() {
+    var alias, clazz, injections;
+    clazz = arguments[0], alias = arguments[1], injections = 3 <= arguments.length ? __slice.call(arguments, 2) : [];
+    injections.push("$scope");
+    window[alias] = function() {
+      var args, controller, key, observer, scope, value, _i, _j, _len, _ref, _results;
+      args = 2 <= arguments.length ? __slice.call(arguments, 0, _i = arguments.length - 1) : (_i = 0, []), scope = arguments[_i++];
+      controller = new clazz();
+      for (key in controller) {
+        value = controller[key];
+        scope[key] = value;
+      }
+      if (typeof scope.initializeInjections === "function") {
+        scope.initializeInjections.apply(scope, args);
+      }
+      scope.$scope = scope;
+      _ref = clazz.$observers || [];
+      _results = [];
+      for (_j = 0, _len = _ref.length; _j < _len; _j++) {
+        observer = _ref[_j];
+        _results.push(new observer(scope));
+      }
+      return _results;
+    };
+    return window[alias].$inject = injections;
+  };
+
+  this.JqueryObserver = (function() {
+
+    function JqueryObserver($scope) {
+      var _this = this;
+      this.$scope = $scope;
+      $(function() {
+        if (typeof _this.onReady === "function") {
+          _this.onReady();
+        }
+        return _this.$scope.$digest();
+      });
+    }
+
+    return JqueryObserver;
+
+  })();
 
   this.autowrap = function(clazz, callback) {
     return function(result) {
-      result.__proto__ = new clazz();
-      if (typeof result.initialize === "function") result.initialize();
-      if (callback) return callback(result);
+      inheritClazz(result, clazz);
+      if (callback) {
+        return callback(result);
+      }
     };
   };
 
