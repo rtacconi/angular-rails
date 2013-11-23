e060596123ba2f403eb9368a0f62a5d17b0932ba
aptx4869 <ling548@gmail.com>
2013-11-23 08:01:41 +0800

    Upgrade to 1.2.2

--- test/angular-rails_test.rb
+++ test/angular-rails_test.rb
@@ -4,11 +4,11 @@ class AngularRailsTest < ActiveSupport::TestCase
   def setup
     @app = Dummy::Application
   end
-  
+
   test "angular.js is found as an asset" do
     assert_not_nil @app.assets["angular.min"]
   end
-	test "angle-up.js is found as an asset" do
+  test "angle-up.js is found as an asset" do
     assert_not_nil @app.assets["angle-up"]
   end
 
--- test/generators/install_generator_test.rb
+++ test/generators/install_generator_test.rb
@@ -6,12 +6,12 @@ require 'mocha'
 class InstallGeneratorTest < Rails::Generators::TestCase
   include GeneratorsTestHelper
   tests Angular::Generators::InstallGenerator
-  
+
   def setup
     mkdir_p "#{destination_root}/app/assets/javascripts"
     cp fixture("application.js"), "#{destination_root}/app/assets/javascripts"
     Rails.application.class.stubs(:name).returns("Dummy::Application")
-    
+
     super
   end
 
@@ -23,7 +23,7 @@ class InstallGeneratorTest < Rails::Generators::TestCase
 
   test "Assert angular directory structure is created" do
     run_generator
-    
+
     %W{controllers filters services widgets}.each do |dir|
       assert_directory "#{angular_path}/#{dir}"
       assert_file "#{angular_path}/#{dir}/.gitkeep"
@@ -35,29 +35,29 @@ class InstallGeneratorTest < Rails::Generators::TestCase
     assert_directory angular_spec_path
     assert_file "#{angular_spec_path}/.gitkeep"
   end
-  
+
 
   test "Assert no gitkeep files are created when skipping git" do
     run_generator [destination_root, "--skip-git"]
-    
+
     %W{controllers filters services widgets}.each do |dir|
       assert_no_file "#{angular_path}/#{dir}/.gitkeep"
     end
-		assert_no_file "#{angular_spec_path}/.gitkeep"
-		assert_no_file "#{assets_path}/templates/.gitkeep"
+    assert_no_file "#{angular_spec_path}/.gitkeep"
+    assert_no_file "#{assets_path}/templates/.gitkeep"
   end
-  
+
   test "Assert application.js require angular.js and angular directory" do
     run_generator
-    
+
     assert_file "app/assets/javascripts/application.js" do |application|
       assert_match /require angular.min(.*)require angle-up(.*)require_tree \.\/angular/m, application
     end
-  end  
-  
+  end
+
   def fixture(file)
     File.expand_path("fixtures/#{file}", File.dirname(__FILE__))
   end
-  
+
 end
 
