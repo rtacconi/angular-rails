54c9c23a8184038af7c5f0aa443017aafc5d6a4d
aptx4869 <ling548@gmail.com>
2013-12-04 07:38:56 +0800

    Upgrade to angular 1.2.3

--- lib/generators/angular/resource_helpers.rb
+++ lib/generators/angular/resource_helpers.rb
@@ -7,23 +7,23 @@ module Angular
         else
           "application"
         end
-    	end
-  		
-			def assets_path
-				"app/assets"
-			end
+      end
 
-			def angular_path
-				"#{assets_path}/javascripts/angular"
-			end 
+      def assets_path
+        "app/assets"
+      end
 
-			def angular_templates_path
-				"#{assets_path}/templates"
-			end 
+      def angular_path
+        "#{assets_path}/javascripts/angular"
+      end
 
-			def angular_spec_path
-				"spec/javascripts/angular"
-			end 
+      def angular_templates_path
+        "#{assets_path}/templates"
+      end
+
+      def angular_spec_path
+        "spec/javascripts/angular"
+      end
     end
   end
 end
